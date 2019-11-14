2fb1aa228ee0c04096a0fde606e02d3900bb464a
Ivandi Santoso <n01398965@nj-00118-008.humber.org>
2019-11-14 15:27:12 -0500

    mapped

--- js/index.js
+++ js/index.js
@@ -10,8 +10,15 @@ const allCourses = [
     weeks: 15,
     breaks: true,
     duration: 160
-  },
-  {
+  },{
+    name: `Applied Web Development`,
+    code: `WDDM-113`,
+    instructor: `Rocco Panacci`,
+    start: { term: `Fall`, year: 2019 },
+    weeks: 15,
+    breaks: true,
+    duration: 160
+  },{
     name: `Web Production Techniques`,
     code: `WDDM-116`,
     instructor: `Milorad Eftoski`,
@@ -35,53 +42,39 @@ function getDurationFromMinutes(minutes) {
   return `${hrs} hr, ${mins} min`;
 }
 
-function setCourseHtml(courseIn){
-  return `<em>${courseIn.name} ${courseIn.code} ${courseIn.instructor} ${courseIn.start.term} ${courseIn.start.year} ${courseIn.weeks} ${courseIn.breaks} ${courseIn.duration}</em>`;
+// const newCourse = prompt(``);
+// allCourses.push(newCourse)
+// Function:
+// Parameters: course:Object
+// Return: String of HTML (<article>)
+function getCourseAsHtmlString(course) {
+  return `
+    <article class="course">
+      <h3 id="name">${course.name}</h3>
+      <ul>
+        <li>Course Code: <strong>${course.code}</strong></li>
+        <li>Instructor: <strong>${course.instructor}</strong></li>
+        <li>Start: <strong>${course.start.term} ${course.start.year}</strong></li>
+        <li>
+          Weeks: <strong>${course.weeks}</strong>
+          <ul>
+            <li>Includes Break: <strong>${course.breaks}</strong></li>
+          </ul>
+        </li>
+        <li>Duration: <strong>${getDurationFromMinutes(course.duration)}</strong></li>
+      </ul>
+    </article>`;
 }
-
-// function setCourseHtml(courseIn){
-//   console.log(`Course name ${courseIn.name} code ${courseIn.code} instructor ${courseIn.instructor} term ${courseIn.start.term} year ${courseIn.start.year} week ${courseIn.weeks} break ${courseIn.breaks} duration ${courseIn.duration}`);
-// }
-
-
-// function getCourseAsHtmlString(){
-  
-// }
-
 
 /************* EXECUTABLE *************/
 // Execute functions that will access data
-
-// document.getElementById(`name`).innerHTML = course.name;
-// document.getElementById(`code`).innerHTML = course.code;
-// document.getElementById(`instructor`).innerHTML = course.instructor;
-// document.getElementById(`start`).innerHTML = `${course.start.term} ${course.start.year}`;
-// document.getElementById(`weeks`).innerHTML = course.weeks;
-// document.getElementById(`duration`).innerHTML = getDurationFromMinutes(course.duration);
+const arrCoursesHtml = allCourses.map(getCourseAsHtmlString);
+const strCoursesHtml = arrCoursesHtml.join(`\n`);
+document.getElementById(`courses`).innerHTML=strCoursesHtml;
 
 
-
-document.getElementById(`course`).innerHTML += setCourseHtml(allCourses[0]);
-document.getElementById(`course`).innerHTML += setCourseHtml(allCourses[1]);
-
-
-
-
-
-// LAB: ARRAYS, PART 1
-// 1. Create a 2nd course Object
-// 2. Store both course Objects in an array whose reference is stored as: allCourses
-//      allCourses is an [] (reference) that hold two identically structured Objects
-// 3. Modify the lines above (32-37) to put the values from the first course into the document
-// 4. Wrap the output code (lines 32-37) in a function called setCourseHtml()
-//      It should take a single Object as a parameter and update the document to match the argument passed
-//      For example:
-//          setCourseHtml( allCourses[0] );   // Will set data from the first course to the document
-//          setCourseHtml( allCourses[1] );   // Will set data from the second course to the document
-//
-// 5. (LATER) Write a function called getCourseAsHtmlString() (it will replace #4, just comment that function)
-//      It should take a single course object as a parameter and return an HTML template string with the course data in it
-//      Ie, Move the HTML for a course (<article>) from index.html to the function in Javascript
-//      Fill it with the parameter Object's data, then return it
-//      At the point it was called, get the return string and append it to <main> (which should be set with id="courses")
-
+// LAB:
+// map() the Array to a new array of Strings (formatted as HTML)
+// join() the NEW Array to a new String of HTML instructions (<article>)
+// Assign the String to the document
+// Test by adding a new Object to allCourses
